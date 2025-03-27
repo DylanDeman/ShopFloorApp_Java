3e9897e6305d89bd42e73ae6034add20deb27357
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-03-27 15:06:46 +0100

    Added columns

--- Java-G12-SDP2/src/main/java/gui/UserManagementPane.java
+++ Java-G12-SDP2/src/main/java/gui/UserManagementPane.java
@@ -3,11 +3,14 @@ package gui;
 import domain.User;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.layout.GridPane;
 
 public class UserManagementPane extends GridPane
 {
+
+	private TableView<User> userTable;
 
 	public UserManagementPane()
 	{
@@ -15,12 +18,28 @@ public class UserManagementPane extends GridPane
 		setHgap(10);
 		setPadding(new Insets(20));
 
-		TableView<User> userTable = new TableView<>();
+		userTable = new TableView<>();
+		buildColumns();
 
 		add(userTable, 0, 0, 2, 1);
 
 		Button addButton = new Button("Gebruiker toevoegen +");
 
 		add(addButton, 0, 1);
+	}
+
+	private void buildColumns()
+	{
+		TableColumn<User, Integer> idColumn = new TableColumn<>("ID");
+
+		TableColumn<User, String> nameColumn = new TableColumn<>("Naam");
+
+		TableColumn<User, String> emailColumn = new TableColumn<>("Email");
+
+		TableColumn<User, String> roleColumn = new TableColumn<>("Rol");
+
+		TableColumn<User, String> statusColumn = new TableColumn<>("Status");
+
+		userTable.getColumns().addAll(idColumn, nameColumn, emailColumn, roleColumn, statusColumn);
 	}
 }
