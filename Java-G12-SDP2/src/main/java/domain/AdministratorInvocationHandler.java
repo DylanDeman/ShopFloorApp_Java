8979012e67e08f49d905e3d65031443d185f497d
SijadWalipoor <Sijad.walipoor@student.hogent.be>
2025-04-01 21:49:44 +0200

    Beginning of authentication & authorisation

---
+++ Java-G12-SDP2/src/main/java/domain/AdministratorInvocationHandler.java
@@ -0,0 +1,31 @@
+package domain;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class AdministratorInvocationHandler implements InvocationHandler {
+	
+	private User user;
+
+	@Override
+	public Object invoke(Object proxy, Method method, Object[] args) throws IllegalAccessException {
+		try {
+			String methodName = method.getName();
+			
+			// Hier moeten de acties komen die de technieker kan uitvoeren 
+			// met de juiste exceptie wanneer hij niet de juiste rol heeft:
+//			if (methodName.startsWith("get")) {
+//                return method.invoke(person, args);
+//            }
+//			if (methodName.equals("setHotOrNotRating")) {
+//                throw new IllegalAccessException();
+//            }
+			method.invoke(user, args);
+		} 
+		catch (InvocationTargetException  e) {
+			e.printStackTrace(); // TODO hier een mooie exceptie teruggeven
+		}
+		return null;
+	}
+}
---
+++ Java-G12-SDP2/src/main/java/gui/login/LoginComponent.java
@@ -0,0 +1,5 @@
+package gui.login;
+
+public class LoginComponent {
+
+}
