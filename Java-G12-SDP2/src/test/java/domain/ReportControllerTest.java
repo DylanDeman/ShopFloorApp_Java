7ee7d16fe732b831ea2c127581552a14ab61a6bf
Milan Dhondt <milan.dhondt@student.hogent.be>
2025-05-21 10:09:03 +0200

    Fixed reportTest

--- Java-G12-SDP2/src/test/java/domain/ReportControllerTest.java
+++ Java-G12-SDP2/src/test/java/domain/ReportControllerTest.java
@@ -1,4 +1,4 @@
-package domain.needsRefactoring;
+package domain;
 
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
